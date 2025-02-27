1b0dd728700ae566fa3334a982fdb7d6ed797c83
DavidCorella <davidcorella.sanchez2511@gmail.com>
2025-02-26 18:49:14 -0600

    Backend Registrar usuarios

---
+++ Backend/GestorMateriasBC/Modelos/Usuario.js
@@ -0,0 +1,33 @@
+
+export default class Usuario{
+    constructor(user, contrasenna, email){
+        this.user = user;
+        this.contrasenna = contrasenna;
+        this.email =  email;
+    }
+
+    GetUser(){
+        return this.user;
+    }
+
+    SetUser(user){
+        this.user = user;
+    }
+
+    GetContrasenna(){
+        return this.contrasenna;
+    }
+
+    SetContrasenna(contrasenna){
+        this.contrasenna = contrasenna;
+    }
+
+    GetEmail(){
+        return this.email;
+    }
+
+    SetEmail(email){
+        this.email = email;
+    }
+
+}
